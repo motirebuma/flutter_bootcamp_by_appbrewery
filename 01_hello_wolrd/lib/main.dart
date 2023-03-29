b1ec18c38f2f14f03f5a2024ca48167014dda9f4
killo <motirebuma7@gmail.com>
2023-03-29 21:02:29 +0300

    01 hello world

---
+++ 01_hello_wolrd/lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Center(
+      child: Text("Hello World"),
+    )
+  ));
+}
