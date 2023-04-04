38678dc3267d638e4cb3c21a670c1fd5d63960ed
killo <motirebuma7@gmail.com>
2023-04-04 21:51:05 +0300

    pubspec

--- mi_card/lib/main.dart
+++ mi_card/lib/main.dart
@@ -12,7 +12,52 @@ class  MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+          child: Column(
+            children: [
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage("/images/killo.jpg"),
+              ),
+              Text(
+                'Killo',
+                style: TextStyle(
+                  fontFamily: 'Pacifico',
+                  fontSize: 40.0,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                'Security Guy',
+                style: TextStyle(
+                  fontFamily: 'Source Sans Pro',
+                  fontSize: 20.0,
+                  color: Colors.teal.shade900,
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: 2.5,
+                ),
+              ),
+              Container(
+                child: Row(
+                  children: [
+                    Icon(
+                      Icons.phone,
+                      color: Colors.white,
+                    ),
+                    Text(
+                      '+132 345 6789',
+                      style: TextStyle(
+                        color: Colors.teal.shade900,
+                        fontFamily:
+                      ),
+                    )
+                  ],
+                )
+              )
+            ],
+          ),
+        )
       )
     );
   }
